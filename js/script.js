2f511a951b6a1dc63a545de5f82ea0c28f8702df
Martin <markizzz88@rambler.ru>
2021-10-07 22:46:13 +0300

    62les

--- js/script.js
+++ js/script.js
@@ -324,6 +324,17 @@ window.addEventListener('DOMContentLoaded', () => {
         width = window.getComputedStyle(slidesWrapper).width;
 
     let slideIndex = 1;
+    let offset = 0;
+
+       if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex; 
+    }
+
+    slidesWrapper.style.overflow = 'hidden';
 
     slidesField.style.width = 100 * slides.length + '%';
     slidesField.style.display = 'flex';
@@ -333,7 +344,53 @@ window.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width;
     });
 
+    next.addEventListener('click', () => {
+            if (offset == +width.slice(0, width.length -2) * (slides.length - 1)){ 
+                offset = 0;
+            } else {
+                offset += +width.slice(0, width.length -2);
+            }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else{
+            current.textContent = slideIndex;
+        }
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0){ 
+            offset = +width.slice(0, width.length -2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length -2);
+        }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+        
+    if (slideIndex == 1) {
+        slideIndex = slides.length;
+    } else {
+        slideIndex--;
+    }
+
+    if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+    } else{
+        current.textContent = slideIndex;
+    }
+});
+
     // showSlides(slideIndex);
+
     // if (slides.length < 10) {
     //     total.textContent = `0${slides.length}`;
     // } else {
